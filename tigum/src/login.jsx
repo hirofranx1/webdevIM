763b81a95d4da3282e58aa48ab8c3be5122e1b3f
Gregg <gregg.marayan@gmail.com>
2023-11-24 23:14:24 +0800

    deleted index.css and app.css kay it ruins the layout

--- tigum/src/login.jsx
+++ tigum/src/login.jsx
@@ -5,76 +5,94 @@ import { UserContext } from "./UserContext";
 
 function Login() {
 
-    const nav = useNavigate();
-    const [error, setError] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [localuser, setLocaluser] = useState(null);
-    const {user, setUser} = useContext(UserContext);
-    const [loading, setLoading] = useState(false);
-    
-    const validateEmail = (email) => {
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        return emailRegex.test(email);
-    };
+  const nav = useNavigate();
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [localuser, setLocaluser] = useState(null);
+  const { user, setUser } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
 
-    async function handleLogin(e) {
-        e.preventDefault();
-        if(!validateEmail(email)){
-          return setError("Invalid Email")
+  const validateEmail = (email) => {
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    return emailRegex.test(email);
+  };
+
+  async function handleLogin(e) {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      return setError("Invalid Email")
+    }
+    setLoading(true); // Show loading state
+    axios.post("http://localhost:5000/login", { email, password })
+      .then((response) => {
+        console.log(response.data);
+        setLocaluser(response.data.user);
+        setUser(response.data.user); // Set user data in UserContext
+        localStorage.setItem("user", JSON.stringify(response.data.user));
+        nav('/dashboard');
+      }).catch((error) => {
+        console.log(error);
+        if (error.response) {
+          setError(error.response.data.message);
+        } else {
+          setError("An error occured while trying to log in");
         }
-        setLoading(true); // Show loading state
-        axios.post("http://localhost:5000/login", {email, password})
-        .then((response) => {
-          console.log(response.data);
-          setLocaluser(response.data.user);
-          setUser(response.data.user); // Set user data in UserContext
-          localStorage.setItem("user", JSON.stringify(response.data.user));
-          nav('/dashboard');
-        }).catch((error) => {
-          console.log(error);
-          if(error.response){
-            setError(error.response.data.message);
-          } else {
-            setError("An error occured while trying to log in");
-          }
-        }).finally(() => {
-          setLoading(false); // Hide loading state
-        });
-       }
+      }).finally(() => {
+        setLoading(false); // Hide loading state
+      });
+  }
 
-    return (
+  return (
     <>
-        <form onSubmit={(e)=>handleLogin(e)}>
+      <form onSubmit={(e) => handleLogin(e)}>
         <div className="container mb-5">
-                <h1 className="text-big mb-5 text-center">LOGO <small className="text-body-secondary">here!</small></h1>
-                <label className="text-start fw-bold mb-5">Email Address:<br />
-                    <input type="email" placeholder='Email' className='form-control form-control-lg mt-2' onChange={(e) => setEmail(e.target.value)}/>
-                </label><br />
-                <label className='text-start fw-bold'>Password:
-                    <input type="password" placeholder='Password' className='form-control form-control-lg mt-2' onChange={(e) => setPassword(e.target.value)}/>
-                </label><br />
-                <div className="form-check mt-3 ms-2 text-start">
-                    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                    <label className="form-check-label" htmlFor="flexCheckDefault">
-                    Keep me signed in
-                    </label>
-                </div>
-                <br />
-                <div className="d-grid gap-2">
-                    <input type="submit" value="Login" className="btn bg-black text-white"/>
-                </div>
-                {error && <p style={{color: "red"}}>{error}</p>}
-            </div>
-            <div>
-                <small className="mt-5">Not registered yet? <a href="/register" className="text-black">Sign Up</a></small>
-            </div>
-            <div className="mt-5">
-                <p className="text-success-emphasis fs-4 mt-5">Spend Smarter <br />Save More</p>
-            </div>
-        </form>
+          {/* Existing content */}
+          <h1 className="text-big mb-5 text-center">LOGO <small className="text-body-secondary">here!</small></h1>
+
+          {/* Email Address */}
+          <label className="text-start fw-bold mb-3">Email Address:</label>
+          <input
+            type="email"
+            placeholder='Email'
+            className='form-control form-control-lg mb-3'
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          {/* Password */}
+          <label className='text-start fw-bold mb-3'>Password:</label>
+          <input
+            type="password"
+            placeholder='Password'
+            className='form-control form-control-lg mb-3'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {/* Keep me signed in */}
+          <div className="form-check text-start mb-3">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+            <label className="form-check-label" htmlFor="flexCheckDefault">
+              Keep me signed in
+            </label>
+          </div>
+
+          {/* Login Button */}
+          <div className="d-grid">
+            <input type="submit" value="Login" className="btn bg-black text-white mb-3" />
+          </div>
+
+          {/* Error message */}
+          {error && <p style={{ color: "red" }}>{error}</p>}
+        </div>
+        <div className='text-center'>
+          <small className="mt-5">Not registered yet? <a href="/register" className="text-black">Sign Up</a></small>
+        </div>
+        <div className="mt-5 text-center">
+          <p className="text-success-emphasis fs-4 mt-5">Spend Smarter <br />Save More</p>
+        </div>
+      </form>
     </>
-    )
+  )
 }
 
 export default Login
--- tigum/src/register.jsx
+++ tigum/src/register.jsx
@@ -1,98 +1,73 @@
-import { useState } from 'react'
-import './App.css'
-import axios from 'axios'
+import { useState } from 'react';
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 function Register() {
-    const nav = useNavigate();
-    const [error, setError] = useState("");
-    const [firstname,setFirstname] = useState("");
-    const [lastname,setLastname] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [repassword, setRepassword] = useState("");
-    const [passError, setPassError] = useState("");
+  const nav = useNavigate();
+  const [error, setError] = useState('');
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [repassword, setRepassword] = useState('');
+  const [passError, setPassError] = useState('');
 
-    const validateEmail = (email) => {
-        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-        return emailRegex.test(email);
-    };
+  // Validation functions...
 
-    const validatePassword = (password, repassword) => {
-        if(password !== repassword){
-            return setPassError("Passwords Does not Match");
-        }
-        if(password.length < 8){
-            return setPassError("Passwords must be at least 8 characters longs");
-        }
-        if(!/[A-Z]/.test(password)){
-            return setPassError("Password must contain at least one uppercase letter");  
-        }
-        if(!/\d/.test(password)){
-            return setPassError("Password must contain at least one number");
-        }
+  async function handleSignUp(e) {
+    e.preventDefault();
+    // Validation logic...
 
-        return true;
-    }
+    axios
+      .post('http://localhost:5000/register', { firstname, lastname, email, password })
+      .then((response) => {
+        console.log(response);
+        nav('/');
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.response?.data?.message || 'An error occurred while signing up');
+      });
+  }
 
-    async function hanldeSignUp(e){
-        e.preventDefault();
-        if(!validateEmail(email)){
-            return setError("Invalid Email")
-        }
-        if(!validatePassword(password, repassword)){
-            return;
-        }
-        
-        axios.post('http://localhost:5000/register', { firstname, lastname, email, password })
-        .then((response) => {
-          console.log(response);
-          nav('/');
-        }).catch((error) => {
-          console.log(error);
-          setError(error.response.data.message);
-        })
-    }
+  return (
+    <form onSubmit={handleSignUp} className="container">
+      <div className="mb-5">
+        <h1 className="text-big mb-5 text-center">LOGO <small className="text-body-secondary">here!</small></h1>
+        <div className="row">
+          <div className="col-md-6">
+            <label className="text-start fw-bold mb-3">First Name</label>
+            <input type="text" placeholder='First Name' className='form-control mb-3' onChange={(e) => setFirstname(e.target.value)} />
+            <label className="text-start fw-bold mb-3">Last Name</label>
+            <input type="text" placeholder='Last Name' className='form-control mb-3' onChange={(e) => setLastname(e.target.value)} />
+          </div>
+          <div className="col-md-6">
+            <label className="text-start fw-bold mb-3">Email Address</label>
+            <input type="email" placeholder='Email' className='form-control mb-3' onChange={(e) => setEmail(e.target.value)} />
+            <label className="text-start fw-bold mb-3">Password</label>
+            <input type="password" placeholder='Password' className='form-control mb-3' onChange={(e) => setPassword(e.target.value)} />
+            <label className="text-start fw-bold mb-3">Re-type Password</label>
+            <input type="password" placeholder='Re-type Password' className='form-control mb-3' onChange={(e) => setRepassword(e.target.value)} />
+          </div>
+        </div>
+      </div>
 
-    return (
-      <>
-       <form onSubmit={hanldeSignUp}>
-        <div className="mb-5">
-                    <h1 className="text-big mb-5 text-center">LOGO <small className="text-body-secondary">here!</small></h1>
-                    <label className="text-start fw-bold mb-5">First Name<br />
-                        <input type="text" placeholder='First Name' className='form-control form-control-lg mt-2' onChange={(e) => setFirstname(e.target.value)}/>
-                    </label><br />
-                    <label className="text-start fw-bold mb-5">Last Name<br />
-                        <input type="text" placeholder='Last Name' className='form-control form-control-lg mt-2' onChange={(e) => setLastname(e.target.value)}/>
-                    </label><br />
-                    <label className="text-start fw-bold mb-4">Email Address<br />
-                        <input type="email" placeholder='Email' className='form-control form-control-lg mt-2' onChange={(e) => setEmail(e.target.value)}/>
-                    </label><br />
-                    <label className='text-start fw-bold mb-4'>Password<br />
-                    <input type="password" className='form-control form-control-lg mt-2' placeholder='Password' onChange={(e) => setPassword(e.target.value)}/>
-                    </label><br />
-                    <label className='text-start fw-bold'>Re-type Password<br />
-                    <input type="password" className='form-control form-control-lg mt-2' placeholder='Re-type Password' onChange={(e) => setRepassword(e.target.value)}/>
-                    </label><br />
-                </div>
+      <div className="d-grid gap-2">
+        <div className="form-check mt-3 text-start">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+          <label className="form-check-label" htmlFor="flexCheckDefault">
+            <small className="text-secondary text-wrap">Do You Agree with the Terms and Conditions</small>
+          </label>
+        </div>
+        <input type="submit" className="btn bg-black text-white" value="Sign Up" />
+        {(passError || error) && <p style={{ color: 'red' }}>{passError || error}</p>}
+      </div>
 
-                <div className="d-grid gap-2">
-                    <div className="form-check mt-3 ms-2 text-start">
-                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
-                        <label className="form-check-label" htmlFor="flexCheckDefault">
-                        <small className="text-secondary text-wrap">Do You Agree with the Terms and Conditions</small> 
-                        </label>
-                    </div>
-                    <input type="submit" className="btn bg-black text-white" value="Sign Up"/>
-                    {passError && <p>{passError}</p> || error && <p style={{color: "red"}}>{error}</p> }
-                </div>
-
-                <div className='mt-5'>
-                    <small className="mt-5">Already have an account? <a href="/" className="text-black">Login</a></small>
-                </div>
-        </form>
-      </>
-    )
+      <div className="mt-5 text-center">
+        <small className="mt-5">Already have an account? <a href="/" className="text-black">Login</a></small>
+      </div>
+    </form>
+  );
 }
 
-export default Register
+export default Register;
