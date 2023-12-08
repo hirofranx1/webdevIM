07b9c25ad777f741d1252783cdba9a7ee72b4846
Gregg <gregg.marayan@gmail.com>
2023-12-08 22:51:16 +0800

    LOGO update 2

--- tigum/src/register.jsx
+++ tigum/src/register.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import logo from './assets/tigum_logos/logopinas.png';
 
 function Register() {
   const nav = useNavigate();
